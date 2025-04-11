3cf07b1aef567410e4bc1d946ac2a8c298aa2124
Yong Fang Chia <yongfang@foqcus.com>
2025-04-11 20:59:30 +0800

    Clean up for homepage

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,113 +1,69 @@
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Fragment, useEffect, useState } from 'react';
+import { Text, View, ScrollView, RefreshControl } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { getBalance } from '../features/wallet/walletAction';
+import { getBalance, getRecentTransaction } from '../features/wallet/walletAction';
 import { Placeholder, PlaceholderLine, Fade } from 'rn-placeholder';
 import ActionButton from '~/components/ActionButton';
-import { Balance, UserProfile } from '~/types';
+import { Balance, TransactionListType, UserProfile } from '~/types';
 import { AppDispatch } from '~/store';
-
-type CategoryType = 'Drink' | 'Market' | 'Transfer';
-
-type Transaction = {
-  category: CategoryType;
-  amount: number;
-  type: 'in' | 'out';
-  remark: string;
-};
-
-type Categories = Record<CategoryType, { icon: string; color: string }>;
+import moment from 'moment';
 
 const HomeScreen: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const [viewBalance, setViewBalance] = useState<boolean>(false);
   const navigation = useNavigation<NavigationProp<any>>();
+  const initialLoaded = useSelector((state: { wallet: { initialLoaded: boolean } }) => state.wallet.initialLoaded);
   const balance = useSelector((state: { wallet: { balance: Balance } }) => state.wallet.balance);
   const profile = useSelector((state: { auth: { profile: UserProfile } }) => state.auth.profile);
+  const recentTransactions = useSelector((state: { wallet: { recentTransaction: TransactionListType } }) => state.wallet.recentTransaction);
 
-  const history: Transaction[] = [
-    {
-      category: 'Drink',
-      amount: 10,
-      type: 'in',
-      remark: 'Starbucks',
-    },
-    {
-      category: 'Market',
-      amount: 10,
-      type: 'in',
-      remark: 'groceries',
-    },
-    {
-      category: 'Transfer',
-      amount: 10,
-      type: 'out',
-      remark: 'John Smith',
-    },
-  ];
+  const [refreshing, setRefreshing] = useState(false);
 
-  const categories: Categories = {
-    Drink: {
-      icon: 'cup',
-      color: '#f2bcff',
-    },
-    Market: {
-      icon: 'cart',
-      color: '#ffea83',
-    },
-    Transfer: {
-      icon: 'arrow-right',
-      color: '#ffaaaa',
-    },
+  const onRefresh = (): void => {
+    setRefreshing(true);
+
+    Promise.all([
+      dispatch(getBalance()),
+      dispatch(getRecentTransaction()),
+    ])
+      .finally(() => {
+        setRefreshing(false);
+      });
   };
 
   useEffect(() => {
-    if (!balance.isLoaded)
+    if (!initialLoaded) {
       dispatch(getBalance());
-  }, [balance]);
+      dispatch(getRecentTransaction());
+    }
+  }, [initialLoaded]);
 
   return (
     <View className={styles.screen}>
-      <View
-        style={{
-          paddingTop: 20,
-          paddingBottom: 10,
-          paddingHorizontal: 20,
-          gap: 10,
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
-      >
+      <View className={styles.header}>
         {profile.name && (
-          <View
-            style={{
-              borderRadius: 50,
-              backgroundColor: '#2773ff',
-              width: 40,
-              height: 40,
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>
+          <View className={styles.profileIcon}>
+            <Text className={styles.profileInitial}>
               {profile.name[0]}
             </Text>
           </View>
         )}
         <View>
-          <Text style={{ fontSize: 16 }}>Hello,</Text>
-          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{profile.name}</Text>
+          <Text className={styles.greetingText}>Hello,</Text>
+          <Text className={styles.profileName}>{profile.name}</Text>
         </View>
       </View>
-      <ScrollView className={styles.container}>
+      <ScrollView
+        className={styles.container}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
         <View className={styles.balanceContainer}>
           {balance.loading ? (
-            <Placeholder Animation={Fade}>
+            <Placeholder Animation={Fade} >
               <PlaceholderLine />
               <PlaceholderLine />
               <PlaceholderLine width={50} />
@@ -122,55 +78,55 @@ const HomeScreen: React.FC = () => {
                 <MaterialCommunityIcons
                   name={viewBalance ? 'eye' : 'eye-off'}
                   size={24}
-                  color='white'
+                  color="white"
                   onPress={() => setViewBalance(!viewBalance)}
                 />
               </View>
             </>
           )}
         </View>
-        <View className='mt-2'>
+        <View className={styles.actionContainer}>
           <View className={styles.actionRow}>
-            <View className='flex-1'>
+            <View className={styles.flex1}>
               <ActionButton
-                color='#e3ebff'
-                icon={<MaterialCommunityIcons name='line-scan' size={24} color='#2773ff' />}
+                color="#dae5ff"
+                icon={<MaterialCommunityIcons name="line-scan" size={24} color="#2773ff" />}
               >
                 <Text className={styles.buttonText}>Scan</Text>
               </ActionButton>
             </View>
-            <View className='flex-1'>
+            <View className={styles.flex1}>
               <ActionButton
-                color='#e3ebff'
-                icon={<MaterialIcons name='add-circle-outline' size={24} color='#2773ff' />}
+                color="#dae5ff"
+                icon={<MaterialIcons name="add-circle-outline" size={24} color="#2773ff" />}
               >
                 <Text className={styles.buttonText}>Add</Text>
               </ActionButton>
             </View>
           </View>
           <View className={styles.actionRow}>
-            <View className='flex-1'>
+            <View className={styles.flex1}>
               <ActionButton
-                color='#e3ebff'
+                color="#dae5ff"
                 icon={
                   <MaterialCommunityIcons
-                    name='arrow-bottom-left-thin'
+                    name="arrow-bottom-left-thin"
                     size={24}
-                    color='#2773ff'
+                    color="#2773ff"
                   />
                 }
               >
                 <Text className={styles.buttonText}>Receive</Text>
               </ActionButton>
             </View>
-            <View className='flex-1'>
+            <View className={styles.flex1}>
               <ActionButton
-                color='#e3ebff'
+                color="#dae5ff"
                 icon={
                   <MaterialCommunityIcons
-                    name='arrow-top-right-thin'
+                    name="arrow-top-right-thin"
                     size={24}
-                    color='#2773ff'
+                    color="#2773ff"
                   />
                 }
                 action={() => navigation.navigate('RecipientSelection')}
@@ -186,38 +142,47 @@ const HomeScreen: React.FC = () => {
             <Text className={styles.seeAllText}>See all</Text>
           </View>
           <View className={styles.transactionList}>
-            {history.map((item, index) => (
-              <View key={index}>
-                <View className={styles.transactionItem}>
-                  <View className={styles.transactionDetails}>
-                    <View
-                      className={styles.transactionIconContainer}
-                      style={{ backgroundColor: categories[item.category]?.color }}
-                    >
-                      <MaterialCommunityIcons
-                        name={
-                          (categories[item.category]
-                            ?.icon as keyof typeof MaterialCommunityIcons.glyphMap) || 'circle'
-                        }
-                        size={20}
-                        color='black'
-                      />
+            {recentTransactions.loading ? (
+              <Placeholder Animation={Fade}>
+                <PlaceholderLine width={80} />
+                <PlaceholderLine />
+                <PlaceholderLine width={50} />
+              </Placeholder>
+            ) : (
+              <View>
+                {recentTransactions.list.map((item, index: number) => (
+                  <Fragment key={index}>
+                    <View className={styles.transactionItem}>
+                      <View className={styles.transactionIcon}>
+                        {item.action === "send" ? (
+                          <Feather name="send" size={20} color="white" />
+                        ) : item.action === "receive" ? (
+                          <MaterialCommunityIcons name="call-received" size={20} color="white" />
+                        ) : item.action === "payment" ? (
+                          <MaterialIcons name="payment" size={20} color="white" />
+                        ) : (
+                          <MaterialIcons name="add" size={20} color="white" />
+                        )}
+                      </View>
+                      <View className={styles.transactionDetails}>
+                        <Text className={styles.transactionDescription}>{item.description}</Text>
+                        <Text className={styles.transactionDate}>
+                          {moment.utc(item.createdAt).local().format('DD MMM YYYY, HH:mm')}
+                        </Text>
+                      </View>
+                      <View className={styles.transactionAmountContainer}>
+                        <Text className={item.type === "credit" ? styles.creditAmount : styles.debitAmount}>
+                          {item.type === "credit" ? "-" : "+"}{item.currency} {item.amount.toFixed(2)}
+                        </Text>
+                      </View>
                     </View>
-                    <View>
-                      <Text className={styles.transactionCategory}>{item.category}</Text>
-                      <Text className={styles.transactionRemark}>{item.remark}</Text>
-                    </View>
-                  </View>
-                  <Text
-                    className={styles.transactionAmount}
-                    style={{ color: item.type === 'in' ? '#4caf50' : '#f44336' }}
-                  >
-                    RM {item.amount}
-                  </Text>
-                </View>
-                {index !== history.length - 1 && <View className={styles.divider} />}
+                    {index != recentTransactions.list.length - 1 && (
+                      <View className={styles.divider} />
+                    )}
+                  </Fragment>
+                ))}
               </View>
-            ))}
+            )}
           </View>
         </View>
       </ScrollView>
@@ -226,27 +191,35 @@ const HomeScreen: React.FC = () => {
 };
 
 const styles = {
-  screen: 'flex-1 bg-white',
-  container: 'p-5 bg-white',
-  balanceContainer: 'bg-[#3b82f6] rounded-lg px-6 py-8 w-full',
-  title: 'text-white text-2xl',
-  amountContainer: 'flex-row mt-5 items-center',
-  balanceText: 'mr-2 text-4xl text-white font-bold',
-  actionRow: 'flex-wrap flex flex-row gap-2 mt-4',
-  button: 'bg-[#e3ebff] rounded-xl px-2 py-4 items-center flex-1 text-xl flex-row justify-center gap-2',
-  buttonText: 'text-xl flex-1 text-center',
-  transactionHistoryContainer: 'mt-7',
-  transactionHeader: 'flex-row justify-between w-full items-end',
-  transactionTitle: 'text-lg',
-  seeAllText: 'text-sm underline text-[#919191]',
-  transactionList: 'mt-2',
-  transactionItem: 'flex-row justify-between mt-2 items-center',
-  transactionDetails: 'flex-row items-center',
-  transactionIconContainer: 'rounded-full p-1',
-  transactionCategory: 'ml-2 text-lg',
-  transactionRemark: 'ml-2 text-sm',
-  transactionAmount: 'text-base',
-  divider: 'h-[1px] bg-[#e0e0e0] my-2 w-full',
+  screen: "flex-1",
+  container: "p-5",
+  header: "pt-5 pb-3 px-5 gap-3 flex-row items-center",
+  profileIcon: "rounded-full bg-blue-600 w-10 h-10 flex-row items-center justify-center",
+  profileInitial: "text-lg text-white font-bold",
+  greetingText: "text-lg",
+  profileName: "text-lg font-bold",
+  balanceContainer: "bg-blue-500 rounded-lg px-6 py-8 w-full",
+  title: "text-white text-2xl",
+  amountContainer: "flex-row mt-5 items-center",
+  balanceText: "mr-2 text-4xl text-white font-bold",
+  actionContainer: "mt-2",
+  actionRow: "flex-wrap flex-row gap-2 mt-4",
+  flex1: "flex-1",
+  buttonText: "text-lg flex-1 text-center",
+  transactionHistoryContainer: "mt-8",
+  transactionHeader: "flex-row justify-between w-full items-end",
+  transactionTitle: "text-lg",
+  seeAllText: "text-sm underline text-gray-500",
+  transactionList: "bg-white p-3 mt-2 rounded-lg",
+  transactionItem: "flex-row py-3 gap-3",
+  transactionIcon: "bg-blue-900 justify-center items-center rounded-full w-9 h-9",
+  transactionDetails: "flex-1",
+  transactionDescription: "text-lg",
+  transactionDate: "text-sm text-gray-400",
+  transactionAmountContainer: "items-end",
+  creditAmount: "text-red-500",
+  debitAmount: "text-green-500",
+  divider: "h-px bg-gray-300 my-2 w-full",
 };
 
 export default HomeScreen;
